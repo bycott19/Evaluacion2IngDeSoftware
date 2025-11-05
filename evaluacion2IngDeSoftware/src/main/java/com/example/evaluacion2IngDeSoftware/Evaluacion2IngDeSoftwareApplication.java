c7a37bc81228c929f8cb96cd22ef2e7b3bf8c610
Bairon <bairon.munoz2201@alumnos.ubiobio.cl>
2025-11-05 17:30:13 -0300

    prueba

---
+++ evaluacion2IngDeSoftware/src/main/java/com/example/evaluacion2IngDeSoftware/Evaluacion2IngDeSoftwareApplication.java
@@ -0,0 +1,14 @@
+package com.example.evaluacion2IngDeSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evaluacion2IngDeSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Evaluacion2IngDeSoftwareApplication.class, args);
+        //Prueba commit
+	}
+
+}
